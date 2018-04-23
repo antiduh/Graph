bca55b2c293218de5b8f14d63b289c2affde2ae1
Kevin Thompson <antiduh@gmail.com>
2018-04-23 02:16:13 -0400

    Forgot to commit the new inlinks/outlinks struct.

---
+++ Graph/Graph.LinkList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    partial class Graph<TNode, TLink>
+    {
+        private struct LinkList
+        {
+            public List<Link> Outlinks { get; private set; }
+
+            public List<Link> Inlinks { get; private set; }
+
+            public void Init()
+            {
+                Outlinks = new List<Link>();
+                Inlinks = new List<Link>();
+            }
+        }
+    }
+}
